3c4138df7dc0be6084f02171edfbd1471df4f0a4
PauloAlexandre21 <paulo.alexandre21@gmail.com>
2024-02-14 12:32:01 -0300

    Primeira versão

---
+++ 5_JavaComSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Saudacao.java
@@ -0,0 +1,19 @@
+package br.com.erudio;
+
+public class Saudacao {
+	
+	private final Long id;
+	private final String content;
+	public Saudacao(Long id, String content) {
+
+		this.id = id;
+		this.content = content;
+	}
+	public Long getId() {
+		return id;
+	}
+	public String getContent() {
+		return content;
+	}
+
+}
